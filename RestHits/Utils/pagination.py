b558be7b2d3bb520e133c7732263296267bbf0af
Damian Ignaczak <vaqmad@icloud.com>
2025-05-21 01:52:47 +0200

    Add default pagination utility class
    Introduce `DefaultPagination` extending DRF's `PageNumberPagination`. This class standardizes pagination with a default page size of 20, maximum of 100.

---
+++ RestHits/Utils/pagination.py
@@ -0,0 +1,7 @@
+# DRF improts
+from rest_framework.pagination import PageNumberPagination
+
+class DefaultPagination(PageNumberPagination):
+    page_size = 20
+    max_page_size = 100
+    page_size_query_param = 'page_size'
