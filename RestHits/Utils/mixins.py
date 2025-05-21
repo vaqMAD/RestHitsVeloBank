85eb6dcdad88e443c856a3694eec5f3c6eec39d0
Damian Ignaczak <vaqmad@icloud.com>
2025-05-21 12:29:56 +0200

    Update docstring in `PermitGetAdminModifyMixin`

--- RestHits/Utils/mixins.py
+++ RestHits/Utils/mixins.py
@@ -2,12 +2,11 @@
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
-
 class PermitGetAdminModifyMixin:
     """
     A mixin that provides a custom `get_permissions` method.
 
-    - For GET requests: does not apply permission classes (returns an empty list) -> Everyone can see the data.
+    - For GET requests: does not apply permission classes (returns an empty list) -> Everyone can see the payload.
     - For all other HTTP methods (e.g., PUT, PATCH, DELETE): It requires
       the user to be authenticated and to be an admin user.
 
