b60cac7b2dfdecd10f919f73a23ce8bc0a61e567
Damian Ignaczak <vaqmad@icloud.com>
2025-05-22 20:52:24 +0200

    Refactor Artist views for schema integration and query safety.
    Integrated schema decorators for enhanced Swagger documentation. Moved querysets into `swagger_safe_queryset`-decorated methods to improve query safety and maintain consistency. Removed `ordering_at` field from Artist list ordering options.

--- Artists/views.py
+++ Artists/views.py
@@ -5,23 +5,31 @@ from rest_framework.filters import OrderingFilter
 # Internal imports
 from .models import Artist
 from .serializers import (ArtistListSerializer, ArtistDetailSerializer, ArtistCreateSerializer)
+from .artists_spectacular_extensions import (ARTIST_LIST_CREATE_SCHEMA, ARTIST_DETAIL_SCHEMA)
+from .filters import ArtistFilter
 from RestHits.Utils.pagination import DefaultPagination
 from RestHits.Utils.mixins import PermitGetAdminModifyMixin
-from .filters import ArtistFilter
 from RestHits.Utils.mixins import CacheListMixin
+from RestHits.Utils.view_helpers import swagger_safe_queryset
 
+
+@ARTIST_LIST_CREATE_SCHEMA
 class ArtistListCreateView(CacheListMixin, PermitGetAdminModifyMixin, generics.ListCreateAPIView):
     """
     GET: List 20 artists with filtering and ordering.
     POST: Create a new artist (admin only).
     """
+    queryset = Artist.objects.none()
     serializer_class = ArtistListSerializer
     pagination_class = DefaultPagination
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = ArtistFilter
-    ordering_fields = ['first_name', 'last_name', 'created_at', 'updated_at']
+    ordering_fields = ['first_name', 'last_name', 'created_at']
     ordering = ['first_name', 'last_name']
-    queryset = Artist.objects.all()
+
+    @swagger_safe_queryset
+    def get_queryset(self):
+        return Artist.objects.all()
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
@@ -29,6 +37,7 @@ class ArtistListCreateView(CacheListMixin, PermitGetAdminModifyMixin, generics.L
         return ArtistListSerializer
 
 
+@ARTIST_DETAIL_SCHEMA
 class ArtistDetailView(PermitGetAdminModifyMixin, generics.RetrieveUpdateDestroyAPIView):
     """
     GET: Retrieve artist details.
@@ -36,4 +45,8 @@ class ArtistDetailView(PermitGetAdminModifyMixin, generics.RetrieveUpdateDestroy
     DELETE: Remove artist (admin only).
     """
     serializer_class = ArtistDetailSerializer
-    queryset = Artist.objects.all()
+    queryset = Artist.objects.none()
+
+    @swagger_safe_queryset
+    def get_queryset(self):
+        return Artist.objects.all()
