b196d71e9f2248880849b38b1a9f9f64d8d992e4
Damian Ignaczak <vaqmad@icloud.com>
2025-05-22 20:51:19 +0200

    Add Spectacular API schema and documentation views
    Integrated DRF Spectacular endpoints for schema generation and API documentation.

--- RestHits/urls.py
+++ RestHits/urls.py
@@ -1,6 +1,8 @@
 # Django imports
 from django.contrib import admin
 from django.urls import path, include
+# Spectacular imports
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 # Internal imports
 from . import api_v1_urls
 
@@ -8,4 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include(api_v1_urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
