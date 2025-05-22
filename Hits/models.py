be31d645a51b561017023717cfe5522da49d9626
Damian Ignaczak <vaqmad@icloud.com>
2025-05-22 12:24:29 +0200

    Add Hit model with fields, relations, and indexes
    Introduce the Hit model to represent music hits, including fields for title, artist, timestamps, and primary key as UUID. Added necessary validations, foreign key to the Artist model, and database indexes for improved query performance.

--- Hits/models.py
+++ Hits/models.py
@@ -1,3 +1,26 @@
+# Python imports
+import uuid
+# Django imports
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+# Internal imports
+from Artists.models import Artist
 
-# Create your models here.
+
+class Hit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(2), MaxLengthValidator(255)])
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['created_at']),
+            models.Index(fields=['artist']),
+        ]
+
+    def __str__(self):
+        return self.title
