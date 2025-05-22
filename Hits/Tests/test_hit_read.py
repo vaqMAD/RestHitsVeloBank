a6905c4a61a7eeaba09e6c4265b7b4f7f1ebfae4
Damian Ignaczak <vaqmad@icloud.com>
2025-05-22 20:50:46 +0200

    Add tests for HitsByArtistView
    Introduced a new HitsByArtistViewTests class to validate API behavior for grouping, sorting, and pagination of hits by artist. Added cache clearing in HitsPaginationFilterOrderingTests to ensure consistent test behavior. Removed unused imports to improve code maintainability.

--- Hits/Tests/test_hit_read.py
+++ Hits/Tests/test_hit_read.py
@@ -1,7 +1,7 @@
 # Python imports
-import uuid
 from datetime import timedelta
 # Django Imports
+from django.core.cache import cache
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
@@ -11,11 +11,6 @@ from rest_framework import status
 from .base import BaseHitAPITestCase
 from Artists.models import Artist
 from Hits.models import Hit
-from Hits.validators import (
-    VALIDATION_ERROR_CODE_HIT_WITH_GIVEN_TITLE_ALREADY_EXIST_FOR_ARTIST,
-    VALIDATION_ERROR_CODE_ARTIST_NOT_FOUND,
-)
-from RestHits.Utils.test_helpers import get_error_code
 
 
 class HitDetailViewTest(BaseHitAPITestCase):
@@ -58,6 +53,7 @@ class HitListViewTest(BaseHitAPITestCase):
 
 class HitsPaginationFilterOrderingTests(BaseHitAPITestCase):
     def setUp(self):
+        cache.clear()
         self.url = reverse('hits_list_create')
         self._create_bulk_hits()
         self._create_named_hits()
@@ -161,3 +157,69 @@ class HitsPaginationFilterOrderingTests(BaseHitAPITestCase):
         returned_last_names = [hit['artist']['last_name'] for hit in response.data['results']]
         expected = sorted([a.last_name, b.last_name, c.last_name], reverse=True)
         self.assertEqual(returned_last_names[:3], expected)
+
+
+class HitsByArtistViewTests(BaseHitAPITestCase):
+    def setUp(self):
+        self.artist_most = Artist.objects.create(first_name='Alan', last_name='Smith')
+        self.artist_few = Artist.objects.create(first_name='Brian', last_name='Jones')
+        self.artist_none = Artist.objects.create(first_name='Charlie', last_name='Adams')
+
+        for i in range(3):
+            Hit.objects.create(artist=self.artist_most, title=f'Most Hit #{i + 1}')
+
+        for i in range(2):
+            Hit.objects.create(artist=self.artist_few, title=f'Few Hit #{i + 1}')
+
+        self.url = reverse('hits_by_artist')
+
+    def test_status_and_structure(self):
+        resp = self.client.get(self.url)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.data
+        self.assertIn('results', data)
+        for artist in data['results']:
+            for field in ('id', 'first_name', 'last_name', 'hit_count', 'hits'):
+                self.assertIn(field, artist)
+
+    def test_grouping_and_sorting(self):
+        resp = self.client.get(self.url)
+        results = resp.data['results']
+
+        top = results[0]
+        self.assertEqual(top['id'], str(self.artist_most.id))
+        self.assertEqual(top['hit_count'], 3)
+        self.assertEqual(len(top['hits']), 3)
+
+        second = results[1]
+        self.assertEqual(second['id'], str(self.artist_few.id))
+        self.assertEqual(second['hit_count'], 2)
+        self.assertEqual(len(second['hits']), 2)
+
+        third = results[2]
+        self.assertEqual(third['id'], str(self.artist_none.id))
+        self.assertEqual(third['hit_count'], 0)
+        self.assertEqual(len(third['hits']), 0)
+
+    def test_tie_breaker_on_same_hit_count(self):
+        cache.clear()
+        a1 = Artist.objects.create(first_name='Xander', last_name='Blake')
+        a2 = Artist.objects.create(first_name='Yvonne', last_name='Adams')
+        Hit.objects.create(artist=a1, title='Solo Hit')
+        Hit.objects.create(artist=a2, title='Solo Hit')
+
+        resp = self.client.get(self.url)
+        results = resp.data['results']
+
+        ones = [a for a in results if a['hit_count'] == 1]
+        self.assertEqual(ones[0]['last_name'], 'Adams')
+        self.assertEqual(ones[1]['last_name'], 'Blake')
+
+    def test_pagination_defaults(self):
+        resp = self.client.get(self.url)
+        data = resp.data
+        for key in ('count', 'next', 'previous', 'results'):
+            self.assertIn(key, data)
+        total_artists = Artist.objects.count()
+        self.assertEqual(data['count'], total_artists)
