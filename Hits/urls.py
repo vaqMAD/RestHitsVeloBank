5159fb7d08a71eeda5b8ef8287a84dc288ffc37c
Damian Ignaczak <vaqmad@icloud.com>
2025-05-22 20:51:36 +0200

    Add route for HitsByArtistView in urls.py

--- Hits/urls.py
+++ Hits/urls.py
@@ -1,9 +1,10 @@
 # Django imports
 from django.urls import path
 # Internal imports
-from .views import (HitListCreateView, HitDetailView)
+from .views import (HitListCreateView, HitDetailView, HitsByArtistView)
 
 urlpatterns = [
     path('', HitListCreateView.as_view(), name='hits_list_create'),
     path('<uuid:pk>/', HitDetailView.as_view(), name='hits_detail'),
+    path('by-artist/', HitsByArtistView.as_view(), name='hits_by_artist')
 ]
