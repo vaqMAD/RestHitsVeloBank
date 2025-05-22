8a217f23a39660bcb1f7a0acb2672fb4888e9c38
Damian Ignaczak <vaqmad@icloud.com>
2025-05-22 13:55:10 +0200

    Add signal imports to app configs
    Ensure `RestHits.Signals.signals` is imported in the `ready` method of both `Artists` and `Hits` app configs.

--- Artists/apps.py
+++ Artists/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ArtistsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Artists'
+
+    def ready(self):
+        import RestHits.Signals.signals
--- Hits/apps.py
+++ Hits/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HitsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Hits'
+
+    def ready(self):
+        import RestHits.Signals.signals
